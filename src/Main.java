aaeb4a6e4cc0f7991bafdbe76f7cf912a3717abd
Marcin <gasiorek.mail@gmail.com>
2023-03-16 12:14:08 +0100

    repair thread interference by adding synchronized in MyProducer and MyConsumer class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Main {
     public static final String EOF = "EOF";
     public static void main(String[] args) {
+
+        List <String> buffer = new ArrayList<>();
+        MyProducer producer = new MyProducer(buffer,ThreadColor.ANSI_YELLOW);
+        MyConsumer consumer1 = new MyConsumer(buffer,ThreadColor.ANSI_PURPLE);
+        MyConsumer consumer2 = new MyConsumer(buffer,ThreadColor.ANSI_CYAN);
+
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
 
 
     }
@@ -25,15 +35,18 @@ class MyProducer implements Runnable {
         for (String num : nums) {
             try {
                 System.out.println(color + "Adding ..." + num);
-                buffer.add(num);
-
+              synchronized (buffer) {
+                  buffer.add(num);
+              }
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer was interrupted");
             }
         }
         System.out.println(color + "Adding EOF and exiting...");
-        buffer.add("EOF");
+        synchronized (buffer){
+            buffer.add("EOF");
+        }
     }
 }
 
@@ -48,15 +61,18 @@ class MyConsumer implements Runnable {
 
     public void run() {
         while (true) {
-            if (buffer.isEmpty()) {
-                continue;
+            synchronized (buffer){
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(Main.EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
             }
-            if (buffer.get(0).equals(Main.EOF)) {
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
-            }
+
         }
     }
 }
