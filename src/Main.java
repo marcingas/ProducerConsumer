9b5502c5313867c51f574ad9ac2fabca6ffa31f3
Marcin <gasiorek.mail@gmail.com>
2023-03-21 11:44:42 +0100

    add thread pools

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -12,14 +13,35 @@ public class Main {
 //        using Lock to prevent thread interference
         ReentrantLock bufferLock = new ReentrantLock();
 
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+
 
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_CYAN + "Im being printed from callable class");
+                return "callable result" ;
+            }
+        });
+        try{
+            System.out.println(future.get());
+
+        }catch (ExecutionException e){
+            System.out.println("WRONG");
+        }catch (InterruptedException e){
+            System.out.println("THread running the task was interrupted");
+        }
+
+        executorService.shutdown();
 
 
     }
