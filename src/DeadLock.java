0e4a5eba0deae0d662b4333cff4201c6588d58de
Marcin <gasiorek.mail@gmail.com>
2023-03-22 09:41:21 +0100

    DeadLockProblem

--- src/DeadLock.java
+++ src/DeadLock.java
@@ -9,7 +9,8 @@ public class DeadLock {
     }
     private static class Thread1 extends Thread{
         public void run(){
-            synchronized (lock1){
+            //prolbem with deadLock will occur when changed lock1-> lock2 here and:
+           synchronized (lock1){
                 System.out.println("Thread 1: Has lock1");
                 try{
                     Thread.sleep(100);
@@ -17,6 +18,7 @@ public class DeadLock {
                 }catch (InterruptedException e){
                 }
                 System.out.println("Thread 1: I'm waiting for lock2");
+               //prolbem with deadLock will occur when changed lock2-> lock1 here
                 synchronized (lock2){
                     System.out.println("THread 1: Has lock1 and lock2");
                 }
