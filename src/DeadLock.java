251fa762a068ba1b876c706c9fc3d32bd0d8b2d5
Marcin <gasiorek.mail@gmail.com>
2023-03-22 09:39:48 +0100

    DeadLockProblem

---
+++ src/DeadLock.java
@@ -0,0 +1,46 @@
+public class DeadLock {
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        new Thread1().start();
+        new Thread2().start();
+
+    }
+    private static class Thread1 extends Thread{
+        public void run(){
+            synchronized (lock1){
+                System.out.println("Thread 1: Has lock1");
+                try{
+                    Thread.sleep(100);
+
+                }catch (InterruptedException e){
+                }
+                System.out.println("Thread 1: I'm waiting for lock2");
+                synchronized (lock2){
+                    System.out.println("THread 1: Has lock1 and lock2");
+                }
+                System.out.println("Thread 1: released lock2");
+            }
+            System.out.println("Thread 1: released lock1. Exiting...");
+        }
+    }
+    private static class Thread2 extends Thread{
+        public void run(){
+            synchronized (lock1){
+                System.out.println("Thread 2: Has lock1");
+                try{
+                    Thread.sleep(100);
+
+                }catch (InterruptedException e){
+                }
+                System.out.println("Thread 2: I'm waiting for lock2");
+                synchronized (lock2){
+                    System.out.println("THread 2: Has lock1 and lock2");
+                }
+                System.out.println("Thread 2: released lock2");
+            }
+            System.out.println("Thread 2: released lock1. Exiting...");
+        }
+    }
+}
